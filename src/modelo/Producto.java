f449cca952692467566b7ae8f5f882374f4102b7
Ruby Berumen <clararubipb15@gmail.com>
2021-05-23 11:52:37 -0500

    Se creó la calse producto

---
+++ src/modelo/Producto.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class Producto {
+	private String idProducto;
+	private String nombre;
+	private double precio;
+	private String descripcion;
+	
+	public Producto(String idProducto, String nombre, double precio, String descripcion) {
+		this.idProducto = idProducto;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripcion = descripcion;
+	}
+
+	public String getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(String idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
