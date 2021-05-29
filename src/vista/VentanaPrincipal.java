73d3b3bf6335eb5b97f231f656e2683073241a7b
Ruby Berumen <clararubipb15@gmail.com>
2021-05-29 14:20:59 -0500

    Se hicieron modificaciones en el menu item

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -1,11 +1,17 @@
 package vista;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 class Login extends JFrame{
 	
@@ -16,6 +22,8 @@ class Login extends JFrame{
 	BufferedImage imagen;
 	JLabel imagen1;
 	JComboBox <String> cmbTipo;
+	Color moradoObscuro = new Color(133,69,107);
+	Color grisClaro = new Color(212,212,212);
 	
 	public Login() throws IOException {
 		getContentPane().setLayout(null);
@@ -54,24 +62,422 @@ class Login extends JFrame{
 		cmbTipo = new JComboBox<String>(tipo);
 		cmbTipo.setBounds(50, 325, 185, 30);
 		cmbTipo.setFont(new Font("Berlin Sans FB", Font.PLAIN, 16));
-		cmbTipo.setBackground(new Color(133,69,107));
-		cmbTipo.setForeground(new Color(212,212,212));
+		cmbTipo.setBackground(moradoObscuro);
+		cmbTipo.setForeground(grisClaro);
 		add(cmbTipo);
 		
 		btnIngresar = new JButton("Ingresar");
 		btnIngresar.setBounds(95, 380, 100, 35);
-		btnIngresar.setBackground(new Color(133,69,107));
-		btnIngresar.setForeground(new Color(212,212,212));
+		btnIngresar.setBackground(moradoObscuro);
+		btnIngresar.setForeground(grisClaro);
 		btnIngresar.setFont(new Font("Berlin Sans FB", Font.PLAIN, 20));
 		add(btnIngresar);	
 		
+	}
+	
+}
+
+
+
+class Ventana extends JFrame{
+	JMenuBar menuBar = new JMenuBar();
+	JMenu ventas, productos, usuarios;
+	JMenuItem altasV,bajasV,cambiosV,consultasV;
+	JMenuItem altasP,bajasP,cambiosP,consultasP;
+	JMenuItem altasU,bajasU,cambiosU,consultasU;
+	JInternalFrame aV,bV,cV,coV;
+	JInternalFrame aP,bP,cP,coP;
+	JInternalFrame aU,bU,cU,coU;
+	JPanel jpAltasV, jpBajasV, jpCambiosV, jpConsultasV;
+	JPanel jpAltasProd, jpBajasProd, jpCambiosProd, jpConsultasProd;
+	JPanel jpAltasU, jpBajasU, jpCambiosU, jpConsultasU;
+	JDesktopPane dp = new JDesktopPane();
+	
+	String controlador = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/proyecto_final";
+	
+	Color moradoObscuro = new Color(133,69,107);
+	Color grisClaro = new Color(212,212,212);
+	Color moradoBajito = new Color(245,220,249);
+
+
+	public Ventana() {
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize(700,500);
+		setLocationRelativeTo(null);
+		setTitle("Sistema ABCC");
+		setVisible(true);
+		
+		
+		ventas = new JMenu("Ventas");
+		productos = new JMenu("Productos");
+		usuarios = new JMenu("Usuarios");
+	
+	//----------------------------------------------Ventas--------------------------------------------
+		
+		altasV = new JMenuItem("Altas");
+		ventas.add(altasV);
+		bajasV = new JMenuItem("Bajas");
+		ventas.add(bajasV);	
+		cambiosV = new JMenuItem("Cambios");
+		ventas.add(cambiosV);	
+		consultasV = new JMenuItem("Consultas");
+		ventas.add(consultasV);	
+	
+		altasV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM ventas");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aV.setVisible(true);	
+				bV.setVisible(false);
+				cV.setVisible(false);
+				coV.setVisible(false);
+				}
+			});
+		
+		bajasV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM ventas");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aV.setVisible(false);	
+				bV.setVisible(true);
+				cV.setVisible(false);
+				coV.setVisible(false);
+				}
+			});
+		
+		cambiosV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM ventas");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aV.setVisible(false);	
+				bV.setVisible(false);
+				cV.setVisible(true);
+				coV.setVisible(false);
+				}
+			});
+		
+		consultasV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM ventas");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aV.setVisible(false);	
+				bV.setVisible(false);
+				cV.setVisible(false);
+				coV.setVisible(true);
+				}
+			});	
+		
+		
+		//------------------------------------------Productos--------------------------------------------------------
+		
+		altasP = new JMenuItem("Altas");
+		productos.add(altasP);
+		bajasP = new JMenuItem("Bajas");
+		productos.add(bajasP);	
+		cambiosP = new JMenuItem("Cambios");
+		productos.add(cambiosP);	
+		consultasP = new JMenuItem("Consultas");
+		productos.add(consultasP);	
+	
+		altasP.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aP.setVisible(true);	
+				bP.setVisible(false);
+				cP.setVisible(false);
+				coP.setVisible(false);
+				}
+			});
+		
+		bajasP.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aP.setVisible(false);	
+				bP.setVisible(true);
+				cP.setVisible(false);
+				coP.setVisible(false);
+				}
+			});
+		
+		cambiosP.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aP.setVisible(false);	
+				bP.setVisible(false);
+				cP.setVisible(true);
+				coP.setVisible(false);
+				}
+			});
+		
+		consultasP.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aP.setVisible(false);	
+				bP.setVisible(false);
+				cP.setVisible(false);
+				coP.setVisible(true);
+				}
+			});	
+		
+		//------------------------------------------Usuarios--------------------------------------------------------
+		
+		altasU = new JMenuItem("Altas");
+		usuarios.add(altasU);
+		bajasU = new JMenuItem("Bajas");
+		usuarios.add(bajasU);	
+		cambiosU = new JMenuItem("Cambios");
+		usuarios.add(cambiosU);	
+		consultasU = new JMenuItem("Consultas");
+		usuarios.add(consultasU);	
+	
+		altasU.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM usuarios");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aU.setVisible(true);	
+				bU.setVisible(false);
+				cU.setVisible(false);
+				coU.setVisible(false);
+				}
+			});
+		
+		bajasU.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aU.setVisible(false);	
+				bU.setVisible(true);
+				cU.setVisible(false);
+				coU.setVisible(false);
+				}
+			});
+		
+		cambiosU.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aU.setVisible(false);	
+				bU.setVisible(false);
+				cU.setVisible(true);
+				coU.setVisible(false);
+				}
+			});
+		
+		consultasU.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+											
+				ResultSetTableModel modeloDatos = null;
+				try {
+					modeloDatos = new ResultSetTableModel(controlador, url, "SELECT * FROM productos");
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+				
+				aU.setVisible(false);	
+				bU.setVisible(false);
+				cU.setVisible(false);
+				coU.setVisible(true);
+				}
+			});	
+		
+		//------------------------------------------------------------------------------------------------------------
+		
+		
+		menuBar.add(ventas);
+		menuBar.add(productos);
+		menuBar.add(usuarios);
+		
+		setJMenuBar(menuBar);
+	
+		
+		//VENTAS-------------------------------------------------------
+		//------Altas--------
+		aV = new JInternalFrame();
+		aV.getContentPane().setLayout(null);
+		aV.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		aV.setSize(500, 300);
+		aV.setTitle("Ventas");
+		JLabel jlTituloAltasV = new JLabel();
+		jlTituloAltasV = titulo("Agregar",jlTituloAltasV);
+		aV.add(jlTituloAltasV);
+		//------Bajas---------
+		bV = new JInternalFrame();
+		bV.getContentPane().setLayout(null);
+		bV.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		bV.setSize(500, 300);
+		bV.setTitle("Ventas");
+		JLabel jlTituloBajasV = new JLabel();
+		jlTituloBajasV = titulo("Eliminar",jlTituloBajasV);
+		bV.add(jlTituloBajasV);
+		
+		//PRODUCTOS-------------------------------------------------------
+		//------Altas--------
+		aP = new JInternalFrame();
+		aP.getContentPane().setLayout(null);
+		aP.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		aP.setSize(534, 338);
+		aP.setTitle("Productos");
+		JLabel jlTituloAltasP = new JLabel();
+		jlTituloAltasP = titulo("Agregar",jlTituloAltasP);
+		aP.add(jlTituloAltasP);
+		//------Bajas---------
+		bP = new JInternalFrame();
+		bP.getContentPane().setLayout(null);
+		bP.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		bP.setSize(500, 300);
+		bP.setTitle("Productos");
+		JLabel jlTituloBajasP = new JLabel();
+		jlTituloBajasP = titulo("Eliminar",jlTituloBajasP);
+		bP.add(jlTituloBajasP);
+		
+		//USUARIOS-------------------------------------------------------
+		//------Altas--------
+		aU = new JInternalFrame();
+		aU.getContentPane().setLayout(null);
+		aU.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		aU.setSize(534, 338);
+		aU.setTitle("Usuarios");
+		JLabel jlTituloAltasU = new JLabel();
+		jlTituloAltasU = titulo("Agregar",jlTituloAltasU);
+		aU.add(jlTituloAltasU);
+		
+		
+	
+		dp.add(aV);
+		dp.add(bV);
+		dp.add(aP);
+		dp.add(bP);
+		dp.add(aU);
+		dp.setBackground(moradoObscuro);
+		dp.setBounds(0, 0, 750, 450);
+		add(dp);
 		
 		
 		
 		
 	}
 	
+	public JLabel titulo(String x, JLabel jl) {
+		jl = new JLabel(x);
+		jl.setBounds(20, 10, 300, 65);
+		jl.setFont(new Font("Moonbright Demo", Font.PLAIN, 70));
+		jl.setForeground(moradoObscuro);
+		
+		return jl;
+	}
+	
+	
+	public JInternalFrame crearIF(JInternalFrame inf, String tab, String op) {
+		inf = new JInternalFrame();
+		inf.getContentPane().setLayout(null);
+		inf.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		inf.setSize(500, 300);
+		inf.setTitle(tab);
+		JLabel jlTitulo = new JLabel();
+		jlTitulo = titulo(op,jlTitulo);
+		inf.add(jlTitulo);
+		
+		return inf;
+	}
+	
 }
+
 
 
 
@@ -82,14 +488,20 @@ public class VentanaPrincipal {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				new Ventana();
+			}
+		});
+		
+		/*SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
 				try {
 					new Login();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-		});
+		});*/
 
 	}
 
